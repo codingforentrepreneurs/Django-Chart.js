acd0853295ef60abb5aa72e8397d405746885d30
CFE <hello@teamcfe.com>
2017-03-02 15:26:03 -0800

    Django + Chart.js

--- src/charts/urls.py
+++ src/charts/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from .views import HomeView
+from .views import HomeView, get_data, ChartData
+
+
 urlpatterns = [
     url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^api/data/$', get_data, name='api-data'),
+    url(r'^api/chart/data/$', ChartData.as_view()),
     url(r'^admin/', admin.site.urls),
 
 ]
--- src/charts/views.py
+++ src/charts/views.py
@@ -1,8 +1,39 @@
+from django.contrib.auth import get_user_model
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.generic import View
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
+
+User = get_user_model()
 
 class HomeView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'charts.html', {})
+        return render(request, 'charts.html', {"customers": 10})
+
+
+
+def get_data(request, *args, **kwargs):
+    data = {
+        "sales": 100,
+        "customers": 10,
+    }
+    return JsonResponse(data) # http response
+
+
+class ChartData(APIView):
+    authentication_classes = []
+    permission_classes = []
+
+    def get(self, request, format=None):
+        qs_count = User.objects.all().count()
+        labels = ["Users", "Blue", "Yellow", "Green", "Purple", "Orange"]
+        default_items = [qs_count, 23, 2, 3, 12, 2]
+        data = {
+                "labels": labels,
+                "default": default_items,
+        }
+        return Response(data)
+
