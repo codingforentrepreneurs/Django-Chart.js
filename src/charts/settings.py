4acc45c9fcebedebeae3675cc5930ce98ad9027f
CFE <hello@teamcfe.com>
2017-03-02 14:40:06 -0800

    Base project setup

--- src/charts/settings.py
+++ src/charts/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'y=c(8s0+^xa4%ak65n$hutr=n4hy4%*h+%34-*#p(9iou0+j(6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'charts.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
